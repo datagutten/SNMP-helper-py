9cb12dd838215c52dd1f39262d0e249c2624170b
Anders Birkenes <datagutten@datagutten.net>
2025-11-15 20:12:35 +0100

    Add snmp library to service name

--- run_tests_docker.sh
+++ run_tests_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 docker compose build --build-arg SNMP_LIBRARY=$1
-docker compose run --rm tests python3 -m unittest tests.compat.test_compat
+docker compose run --rm tests_$1 python3 -m unittest tests.compat.test_compat
