e38798724fcb54eeef7262c038c7289f64ba3b2b
Anders Birkenes <datagutten@datagutten.net>
2026-06-15 15:24:44 +0200

    Add SNMP v3 args

--- tests/compat/test_compat.py
+++ tests/compat/test_compat.py
@@ -9,6 +9,11 @@ SNMPSession = compat.select(os.getenv('SNMP_LIBRARY'))
 print('Running tests with SNMP library %s' % os.getenv('SNMP_LIBRARY'))
 
 snmpsim_host = os.getenv('SNMPSIM_HOST')
+snmp_version = int(os.getenv('SNMP_VERSION', 2))
+if snmp_version == 3:
+    args = {'security_username': 'testing', 'auth_passphrase': 'testing123', 'security_level': 'authNoPriv'}
+else:
+    args = {}
 
 
 class SNMPTestCase(unittest.TestCase):
@@ -48,67 +53,72 @@ class SNMPTestCase(unittest.TestCase):
         self.assertGreaterEqual(diff.seconds, 3)
 
     def test_empty_string(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.get('.1.3.6.1.2.1.31.1.1.1.18.1')
         self.assertEqual('', response.typed_value())
 
     def test_timeticks(self):
-        session = SNMPSession(snmpsim_host, 'foreignformats/linux')
+        session = SNMPSession(snmpsim_host, 'foreignformats/linux', **args)
         response = session.get('1.3.6.1.2.1.1.3.0').typed_value()
         self.assertIsInstance(response, datetime.timedelta)
         self.assertEqual(14, response.days)
 
     def test_string(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.get('.1.3.6.1.2.1.1.1.0').typed_value()
         self.assertEqual('Linux zeus 4.8.6.5-smp #2 SMP Sun Nov 13 14:58:11 CDT 2016 i686', response)
 
+    # def test_string_v3(self):
+    #     session = SNMPSession.connect_v3(snmpsim_host, 'public', 'testing', 'testing123')
+    #     response = session.get('.1.3.6.1.2.1.1.1.0').typed_value()
+    #     self.assertEqual('Linux zeus 4.8.6.5-smp #2 SMP Sun Nov 13 14:58:11 CDT 2016 i686', response)
+
     def test_int(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.get('.1.3.6.1.2.1.1.7.0').typed_value()
         self.assertEqual(72, response)
 
     def test_counter(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.get('.1.3.6.1.2.1.4.10.0').typed_value()
         self.assertEqual(15835620, response)
 
     def test_gauge(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.get('.1.3.6.1.2.1.2.2.1.5.2').typed_value()
         self.assertEqual(100000000, response)
 
     def test_mac(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.get('.1.3.6.1.2.1.2.2.1.6.2')
         self.assertEqual('00127962f940', response.hex_string())
         self.assertEqual('00127962f940', response.typed_value())
 
     def test_ip(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.get_next('.1.3.6.1.2.1.4.20.1.1')
         self.assertEqual('127.0.0.1', response.ip_address())
         self.assertEqual('127.0.0.1', response.typed_value())
 
     def test_cdp_ip(self):
-        session = SNMPSession(snmpsim_host, 'cisco')
+        session = SNMPSession(snmpsim_host, 'cisco', **args)
         response = session.get('.1.3.6.1.4.1.9.9.23.1.2.1.1.4.10122.4')
         self.assertEqual('10.0.2.242', response.ip_address())
 
     def test_get_next(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.get_next('.1.3.6.1.2.1.1.4.0')
         self.assertEqual('.1.3.6.1.2.1.1.5.0', response.oid)
         self.assertEqual(response.typed_value(), 'zeus.pysnmp.com (you can change this!)')
 
     def test_walk(self):
-        session = SNMPSession(snmpsim_host, 'public')
+        session = SNMPSession(snmpsim_host, 'public', **args)
         response = session.walk('.1.3.6.1.2.1.1')
         self.assertEqual(len(response), 32)
         self.assertEqual(response[31].oid, '.1.3.6.1.2.1.1.9.1.4.8')
 
     def test_multiline(self):
-        session = SNMPSession(snmpsim_host, 'cisco')
+        session = SNMPSession(snmpsim_host, 'cisco', **args)
         response = session.get('.1.3.6.1.2.1.1.1.0')
         desc = (
             "Cisco IOS Software, C2960S Software (C2960S-UNIVERSALK9-M), Version 15.0(2)SE11, RELEASE SOFTWARE (fc3)\r\n"
